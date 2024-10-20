51aa6141dd618fe38df1ae81710120a421146535
arseniypom <arpomazkov@bk.ru>
2024-10-20 15:08:09 +0300

    Deleted combined log file

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -9,7 +9,6 @@ const logger = createLogger({
   transports: [
     new transports.Console(),
     new transports.File({ filename: 'error.log', level: 'error' }),
-    new transports.File({ filename: 'combined.log' }),
   ],
 });
 
