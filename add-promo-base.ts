87afada45ce578f05587a10321ded2710ef8ef37
arseniypom <arpomazkov@bk.ru>
2024-11-26 19:06:03 +0300

    Added promocode creation base

---
+++ add-promo-base.ts
@@ -0,0 +1,26 @@
+import 'dotenv/config';
+import dayjs from 'dayjs';
+import mongoose from 'mongoose';
+import Promocode from './db/Promocode';
+
+async function addPromocodeToDB() {
+  try {
+    await mongoose.connect(process.env.MONGO_DB_URI_DEV!);
+
+    await Promocode.create({
+      code: 'TEST',
+      tokenAmount: 50,
+      timesUsedLimit: 1,
+      validUntil: dayjs(),
+    });
+
+    console.log('Promocode added successfully');
+  } catch (error) {
+    console.error('Error adding promocode:', error);
+  } finally {
+    await mongoose.disconnect();
+  }
+}
+
+addPromocodeToDB();
+
