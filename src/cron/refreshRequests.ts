84a4f5daa55c318c23947fb78a8280a2d501152d
arseniypom <arpomazkov@bk.ru>
2024-12-10 12:26:53 +0300

    Added debug logs

--- src/cron/refreshRequests.ts
+++ src/cron/refreshRequests.ts
@@ -11,26 +11,39 @@ const BATCH_SIZE = 100;
 const BATCH_DELAY_MS = 1000;
 
 // Schedule the task to run every day at 22:00 UTC
-cron.schedule('0 22 * * *', async () => {
+cron.schedule('30 09 * * *', async () => {
   console.log('Current time:', dayjs().format('HH:mm'));
   console.log('running refresh requests cron job');
 
   try {
     const users = await User.find();
     const totalUsers = users.length;
+    console.log('totalUsers', totalUsers);
+
     let processedUsers = 0;
 
     while (processedUsers < totalUsers) {
       const batch = users.slice(processedUsers, processedUsers + BATCH_SIZE);
+      let usersUpdated = 0;
 
       for (const user of batch) {
         const subscriptionData = SUBSCRIPTIONS[user.subscriptionLevel];
         if (subscriptionData.basicRequestsPerDay) {
           user.basicRequestsLeftToday =
             subscriptionData.basicRequestsPerDay || 0;
+          usersUpdated++;
         }
         if (subscriptionData.basicRequestsPerWeek) {
-          if (dayjs().isSame(user.weeklyRequestsExpiry, 'day') || dayjs().isAfter(user.weeklyRequestsExpiry, 'day')) {
+          console.log(
+            user.telegramId,
+            dayjs().isSame(user.weeklyRequestsExpiry, 'day'),
+            dayjs().isAfter(user.weeklyRequestsExpiry, 'day'),
+          );
+
+          if (
+            dayjs().isSame(user.weeklyRequestsExpiry, 'day') ||
+            dayjs().isAfter(user.weeklyRequestsExpiry, 'day')
+          ) {
             user.basicRequestsLeftThisWeek =
               subscriptionData.basicRequestsPerWeek;
             user.weeklyRequestsExpiry = dayjs().add(7, 'day').toDate();
@@ -42,12 +55,14 @@ cron.schedule('0 22 * * *', async () => {
                 reply_markup: profileAddSubscriptionKeyboard,
               },
             );
+            usersUpdated++;
           }
         }
         await user.save();
       }
 
       processedUsers += BATCH_SIZE;
+      console.log(`Updated ${usersUpdated} users`);
       console.log(`Processed ${processedUsers} of ${totalUsers} users`);
 
       if (processedUsers < totalUsers) {
@@ -61,7 +76,7 @@ cron.schedule('0 22 * * *', async () => {
       /block/.test(error.description)
     ) {
       await setUserBlocked(error.payload.chat_id as number);
-      return
+      return;
     }
     logError({
       message: 'Error fetching users',
