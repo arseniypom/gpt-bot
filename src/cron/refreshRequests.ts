5f6704a2f701023101bed95f180461de38b1542e
arseniypom <arpomazkov@bk.ru>
2024-12-10 14:58:19 +0300

    Fixed handling users who blocked bot

--- src/cron/refreshRequests.ts
+++ src/cron/refreshRequests.ts
@@ -11,39 +11,26 @@ const BATCH_SIZE = 100;
 const BATCH_DELAY_MS = 1000;
 
 // Schedule the task to run every day at 22:00 UTC
-cron.schedule('19 11 * * *', async () => {
+cron.schedule('00 12 * * *', async () => {
   console.log('Current time:', dayjs().format('HH:mm'));
   console.log('running refresh requests cron job');
 
   try {
     const users = await User.find();
     const totalUsers = users.length;
-    console.log('totalUsers', totalUsers);
 
     let processedUsers = 0;
     let usersUpdated = 0;
-    let basicRequestsPerDayUpdated = 0;
-    let basicRequestsPerWeekUpdated = 0;
 
     while (processedUsers < totalUsers) {
-      console.log('processedUsers', processedUsers);
-      console.log('-------------------------');
-
       const batch = users.slice(processedUsers, processedUsers + BATCH_SIZE);
-      console.log('batch.length', batch.length);
-      console.log('-------------------------');
 
       for (const user of batch) {
         const subscriptionData = SUBSCRIPTIONS[user.subscriptionLevel];
-        if (processedUsers % 100 === 0) {
-          console.log('processedUsers % 100 === 0', processedUsers);
-          console.log('-------------------------');
-        }
         if (subscriptionData.basicRequestsPerDay) {
           user.basicRequestsLeftToday =
             subscriptionData.basicRequestsPerDay || 0;
           usersUpdated++;
-          basicRequestsPerDayUpdated++;
         }
         if (subscriptionData.basicRequestsPerWeek) {
           if (
@@ -53,16 +40,27 @@ cron.schedule('19 11 * * *', async () => {
             user.basicRequestsLeftThisWeek =
               subscriptionData.basicRequestsPerWeek;
             user.weeklyRequestsExpiry = dayjs().add(7, 'day').toDate();
-            await bot.api.sendMessage(
-              user.telegramId,
-              `*Ваша недельная квота запросов обновилась ✔️ *\n\nНачислено *${subscriptionData.basicRequestsPerWeek} запросов к базовой модели*`,
-              {
-                parse_mode: 'MarkdownV2',
-                reply_markup: profileAddSubscriptionKeyboard,
-              },
-            );
+
+            try {
+              await bot.api.sendMessage(
+                user.telegramId,
+                `*Ваша недельная квота запросов обновилась ✔️ *\n\nНачислено *${subscriptionData.basicRequestsPerWeek} запросов к базовой модели*`,
+                {
+                  parse_mode: 'MarkdownV2',
+                  reply_markup: profileAddSubscriptionKeyboard,
+                },
+              );
+            } catch (error) {
+              if (
+                error instanceof GrammyError &&
+                error.error_code === 403 &&
+                /block/.test(error.description)
+              ) {
+                await setUserBlocked(error.payload.chat_id as number);
+              }
+            }
+
             usersUpdated++;
-            basicRequestsPerWeekUpdated++;
           }
         }
         await user.save();
@@ -77,19 +75,7 @@ cron.schedule('19 11 * * *', async () => {
     }
 
     console.log(`Updated ${usersUpdated} users`);
-    console.log('-------------------------');
-    console.log('basicRequestsPerDayUpdated', basicRequestsPerDayUpdated);
-    console.log('basicRequestsPerWeekUpdated', basicRequestsPerWeekUpdated);
-    console.log('-------------------------');
   } catch (error) {
-    if (
-      error instanceof GrammyError &&
-      error.error_code === 403 &&
-      /block/.test(error.description)
-    ) {
-      await setUserBlocked(error.payload.chat_id as number);
-      return;
-    }
     logError({
       message: 'Error fetching users',
       error,
