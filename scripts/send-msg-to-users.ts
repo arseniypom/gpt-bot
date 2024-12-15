18b5a746e87be48ba1103b38b59df5f7a293be08
arseniypom <arpomazkov@bk.ru>
2024-12-15 14:09:40 +0300

    Updated send message template

--- scripts/send-msg-to-users.ts
+++ scripts/send-msg-to-users.ts
@@ -3,45 +3,92 @@ import mongoose from 'mongoose';
 import User from '../db/User';
 import fs from 'fs';
 import axios from 'axios';
+import { SubscriptionLevels } from '../src/types/types';
+import { BUTTON_LABELS } from '../src/utils/consts';
+import FormData from 'form-data';
+import pLimit from 'p-limit';
 
 export async function sendMessageToAllUsers() {
   const botApiKey = process.env.BOT_API_KEY_DEV;
 
-  try {
-    await mongoose.connect(process.env.MONGO_DB_URI_DEV!);
-    console.log('Mongoose connected');
+  await mongoose.connect(process.env.MONGO_DB_URI_DEV!);
+  console.log('Mongoose connected');
 
-    const users = await User.find({ isBlockedBot: false });
+  const users = await User.find({ isBlockedBot: false });
 
-    let sentMessages = 0;
-    for (const user of users) {
-      user.tokensBalance += 30;
-      await user.save();
+  console.log(users.length);
+  
 
-      const photoStream = fs.createReadStream('img.webp');
-      const headers = {
-        'Content-Type': 'multipart/form-data',
-      };
-      const caption = `*🗣️ Обновление – голосовые сообщения\\!*\n\nТеперь Вы можете отправлять боту голосовые вместо долгого набора текстового сообщения\\! Попробуйте – дарим 30 токенов на баланс 😎\n\nВаш обновленный баланс: ${user.tokensBalance} 🪙`;
+  const limit = pLimit(10);
+  let sentMessages = 0;
 
-      await axios.post(
-        `https://api.telegram.org/bot${botApiKey}/sendPhoto`,
-        {
-          chat_id: user.telegramId,
-          photo: photoStream,
-          caption,
-          parse_mode: 'MarkdownV2',
-        },
-        { headers },
-      );
+  const sendPhotoPromises = users.map((user) => {
+    return limit(async () => {
+      if (user.telegramId === Number(process.env.ADMIN_TELEGRAM_ID)) {
+        const form = new FormData();
+        form.append('chat_id', user.telegramId);
+        form.append('photo', fs.createReadStream('img1.webp'));
+        form.append(
+          'caption',
+          `*🗣️ Встречайте: голосовые сообщения\\!*\n\n→ Теперь можно __отправлять боту голосовые вместо долгого набора текста__\\!\nОчень удобно и быстро\\.\n\n_Данная функция доступна только для пользователей с подпиской – они всегда получают обновления первыми ⚡_`,
+        );
+        form.append('parse_mode', 'MarkdownV2');
 
-      sentMessages++;
-    }
-    console.log(`Sent ${sentMessages} messages`);
-  } catch (error) {
-    console.error('Error sending messages to users:', error);
-  } finally {
-    await mongoose.disconnect();
-    console.log('Mongoose disconnected');
-  }
+        if (user.subscriptionLevel === SubscriptionLevels.FREE) {
+          let keyboard;
+          if (user.canActivateTrial) {
+            keyboard = [
+              [
+                {
+                  text: '🔥 3 дня за 1 рубль',
+                  callback_data: `${SubscriptionLevels.OPTIMUM_TRIAL}-promo`,
+                },
+              ],
+            ];
+          } else {
+            keyboard = [
+              [
+                {
+                  text: BUTTON_LABELS.subscribe,
+                  callback_data: 'subscription',
+                },
+              ],
+            ];
+          }
+
+          form.append(
+            'reply_markup',
+            JSON.stringify({
+              inline_keyboard: keyboard,
+            }),
+          );
+        }
+
+        try {
+          await axios.post(
+            `https://api.telegram.org/bot${botApiKey}/sendPhoto`,
+            form,
+            {
+              headers: {
+                ...form.getHeaders(),
+              },
+            },
+          );
+          sentMessages++;
+        } catch (error) {
+          console.error(
+            `Error sending message to user ${user.telegramId}:`,
+            // @ts-ignore
+            error.response ? error.response.data : error.message,
+          );
+        }
+      }
+    });
+  });
+
+  await Promise.all(sendPhotoPromises);
+
+  console.log(`Sent ${sentMessages} messages`);
+  await mongoose.disconnect();
+  console.log('Mongoose disconnected');
 }
